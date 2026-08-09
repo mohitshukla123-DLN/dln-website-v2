9a604788160adb60b630b0c2b1e2869a848c0dfc
mshukla <mshukla@INSUPMACTRMS.local>
2026-08-09 12:31:14 +0530

    Remove unused product data and services

--- app/data/products/index.ts
+++
@@ -1,9 +0,0 @@
-import { kurtiProducts } from "./kurti";
-import { sareeProducts } from "./saree";
-import { jacketProducts } from "./jacket";
-
-export const products = [
-  ...kurtiProducts,
-  ...sareeProducts,
-  ...jacketProducts,
-];
--- app/data/products/jacket.ts
+++
@@ -1,3 +0,0 @@
-import type { Product } from "../../types/product";
-
-export const jacketProducts: Product[] = [];
