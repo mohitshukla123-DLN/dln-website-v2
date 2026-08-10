f6e3d3e1851b76cfff50a372de3f6a40364f6d2c
mshukla <mshukla@INSUPMACTRMS.local>
2026-08-11 02:47:40 +0530

    refine best sellers grid

--- app/components/homepage/BestSellers.tsx
+++ app/components/homepage/BestSellers.tsx
@@ -59,7 +59,7 @@ export default function BestSellers({ settings }: Props) {
 
         </div>
 
-        <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
 
           {bestSellers.map((product) => (
             <ProductCard
