23a16f2044bb3e2d88f6a087563823d185a10344
mshukla <mshukla@INSUPMACTRMS.local>
2026-08-14 15:58:59 +0530

    Remove unused React Query dependency

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import { QueryClientProvider } from "@tanstack/react-query";
 import App from "../app/App";
-import { queryClient } from "../app/lib/queryClient";
 import { initializeAnalytics } from "../app/lib/analytics";
 import { initializeClarity } from "../app/lib/clarity";
 import "../app/styles/globals.css";
@@ -23,11 +21,9 @@ initializeClarity();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </QueryClientProvider>
     </HelmetProvider>
   </React.StrictMode>
 );
